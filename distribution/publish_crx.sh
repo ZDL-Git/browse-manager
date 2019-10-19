5079821d092d97e60bd9d4cce898a037693baa8f
ZDL <zdl_daily@163.com>
2019-10-19 16:20:54 +0800

    refine publish_crx.sh

--- distribution/publish_crx.sh
+++ distribution/publish_crx.sh
@@ -4,26 +4,41 @@ project_name="browse-manager"
 work_path=$(dirname $0)
 tmp_path=".tmp4publish"
 
-if git status --porcelain | grep -v "??"; then
-    echo "有未提交的代码，请先完成push！"
-    exit 1
+confirm(){
+    echo "===Continue (y/n)?"
+    read choice
+    case "$choice" in
+      y|Y )
+        echo "===continuing..."
+        ;;
+      * )
+        echo "===终止..."
+        exit 1
+        ;;
+    esac
+}
+
+if git cherry -v; then
+    echo "===有未提交或push的代码"
+    confirm
 fi
 
-rm -rfv ${work_path}/${tmp_path}; mkdir ${tmp_path}
+rm -rf ${work_path}/${tmp_path}; mkdir ${tmp_path}
+
+#为了保证源码和发布版本的严格统一，从github库clone代码
 git clone --depth 1 https://github.com/ZDL-Git/${project_name} ${work_path}/${tmp_path}/${project_name}
-
+rm -rf ${work_path}/${tmp_path}/${project_name}/distribution
 version=`cat ${tmp_path}/${project_name}/manifest.json | jq -r '.version'`
-echo "当前版本号 ${version}"
-
+echo "===当前版本号 ${version}"
 if [[ -f ${work_path}/${tmp_path}/${project_name}/distribution/crx/${project_name}-${version}.crx ]]
 then
-    echo "已发布过此版本号"
-    exit 1
+    echo "===已发布过此版本号"
+    confirm
 fi
 
-rm -rf ${work_path}/${tmp_path}/${project_name}/distribution
-
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=${work_path}/${tmp_path}/${project_name} --pack-extension-key=${work_path}/${project_name}.pem --no-message-box
-mv ${work_path}/${tmp_path}/${project_name}.crx ${work_path}/${project_name}-${version}.crx
 
+mv ${work_path}/${tmp_path}/${project_name}.crx ${work_path}/${project_name}-${version}.crx
 rm -rf ${work_path}/${tmp_path}
+
+echo "===完成"
